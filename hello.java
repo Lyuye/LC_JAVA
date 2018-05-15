65966f029f73a67610489815068dad203a4e2afd
lyuye <niuly001@gmail.com>
2018-05-14 17:55:54 -0700

    aci: strStr, naive

--- hello.java
+++ hello.java
@@ -1,10 +1,12 @@
 package com.lc.lyuye;
 
 import com.lc.lyuye.array.*;
+import com.lc.lyuye.string.*;
 
 public class hello {
     public static void main(String [] args){
-        aaa_twosum.test();
-        abh_LongestConsecutiveSub.main();
+        aab_twosum.test();
+        bci_LongestConsecutiveSub.main();
+        aci_strStr.main();
     }
 }
